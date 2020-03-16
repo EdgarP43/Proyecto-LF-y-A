786f6e291e9d43308100f652f09c84e4574a39d5
EdgarP43 <e.paredes-cas@outlook.com>
2020-03-15 19:50:51 -0600

    Fase 1 sin arreglar

---
+++ src/proyectolfya/ProyectoLFyA.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectolfya;
+
+import Classes.*;
+import java.util.ArrayList;
+/**
+ *
+ * @author Edgar
+ */
+public class ProyectoLFyA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Automata archivo;
+        archivo = new Automata("C:\\Users\\Edgar\\Desktop\\New folder\\Archivo Prueba.txt");
+        System.out.println(archivo.getPrintText());
+    }
+    
+}
